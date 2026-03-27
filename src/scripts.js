f2af8736f368be13a26cc00eb7f8bc70227bf34d
Vallarasu Sambathkumar <vals@live.in>
2026-03-27 08:21:49 +0530

    sign up

--- src/scripts.js
+++ src/scripts.js
@@ -11,3 +11,36 @@ function login() {
         redirect("index.html")
     }
 }
+
+function signup() {
+
+    function next() {
+
+    }
+
+    function submit() {
+
+    }
+
+    function populate() {
+        day_select = document.getElementById("signup-dob-day")
+
+        for (let day = 1; day <= 31; day++) {
+            option = document.createElement("option")
+            value = document.createTextNode(`${String(day).padStart(2, 0)}`)
+            option.appendChild(value)
+            day_select.appendChild(option)
+        }
+
+        month_select = document.getElementById("signup-dob-month")
+
+        for (let day = 1; day <= 12; day++) {
+            option = document.createElement("option")
+            value = document.createTextNode(`${String(day).padStart(2, 0)}`)
+            option.appendChild(value)
+            month_select.appendChild(option)
+        }
+    }
+
+    return { next, submit, populate }
+}
